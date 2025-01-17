50c01c0455448e274ec4d2ad99b3b0401aaf41cb
AlisyaAfiz <23018913@myrp.edu.sg>
2025-01-17 13:44:50 +0800

    Updates

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState, useEffect} from 'react';
+import { FlatList, StatusBar, Text, TextInput, View, StyleSheet} from 'react-native';
 
-export default function App() {
+const styles = StyleSheet.create({
+    title: {
+        textAlign: 'center',
+        fontSize: 50,
+        fontWeight:'bold',
+        padding: 20,
+        fontFamily:'cursive',
+        color: '#614444',
+        backgroundColor: "#e4fbff"
+    },
+    boxes: {
+        borderWidth: 2,
+        margin: 10,
+        padding: 15,
+        borderRadius: 10,
+        borderColor: '#614444',
+        backgroundColor: "#efd5d5"
+    },
+    textBox: {
+        borderWidth: 1,
+        padding: 12,
+        backgroundColor: "white",
+        borderColor: '#614444'
+    }
+});
+
+let originalData =[];
+
+const App = () => {
+  const [mydata, setMydata] = useState([]);
+
+  useEffect(() => {
+    fetch("https://mysafeinfo.com/api/data?list=benandjerryflavors&format=json&case=default")
+        .then((response)=>{
+          return response.json();
+        })
+        .then((myJson)=>{
+          if(originalData.length<1) {
+            setMydata(myJson);
+            originalData=myJson;
+          }
+        })
+  }, []);
+
+  const FilterData = (text) => {
+    if(text!='') {
+      let myFilteredData = originalData.filter((item) =>
+          item.Flavor.toLowerCase().includes(text.toLowerCase()) ||
+          item.Description.toLowerCase().includes(text.toLowerCase()));
+      setMydata(myFilteredData);
+    }
+    else {
+      setMydata(originalData);
+    }
+  }
+
+  const renderItem = ({item, index}) => {
+    return (
+        <View style={styles.boxes}>
+          <Text style={{fontSize: 16}}>{item.Flavor}</Text>
+          <Text style={{fontSize: 12}}>{item.Description}</Text>
+        </View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={{backgroundColor: "#e3fdd8"}}>
+        <Text style={styles.title}>Ben and Jerry</Text>
+        <StatusBar/>
+          <View style={{margin: 14}}>
+            <Text style={{color: '#614444'}}>Search:</Text>
+            <TextInput style={styles.textBox} placeholder="Search for your favorite flavors..." onChangeText={(text)=>{FilterData(text)}}/>
+          </View>
+        <FlatList data={mydata} renderItem={renderItem} />
+      </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
